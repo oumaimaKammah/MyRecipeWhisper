1f095c228ce1e431a91ab2c38cac902720014679
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-01-14 13:50:25 +0100

    chore : add initial spring boot backend project generated with Spring Initializr

---
+++ backend/src/main/java/com/myrecipewhisper/backend/MyRecipeWhisperBackendApplication.java
@@ -0,0 +1,13 @@
+package com.myrecipewhisper.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyRecipeWhisperBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyRecipeWhisperBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/myrecipewhisper/backend/MyRecipeWhisperBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myrecipewhisper.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyRecipeWhisperBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
