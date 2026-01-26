a0fe4823e4dbc7661f31117f748835beb7ed2a52
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-01-26 13:49:14 +0100

    feat(dto): add validation rules to CreateUserDTO including strong password regex, email format and non-empty fields.

--- backend/src/main/java/com/myrecipewhisper/backend/dtos/CreateUserDTO.java
+++ backend/src/main/java/com/myrecipewhisper/backend/dtos/CreateUserDTO.java
@@ -2,9 +2,17 @@ package com.myrecipewhisper.backend.dtos;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public record CreateUserDTO(
-        String username,
-        String email,
-        String password,
-        LocalDate birthday) {
+                @NotBlank String username,
+
+                @NotBlank @Email String email,
+
+                @Size(min = 8, message = "Password must be at least 8 characters long") @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!?#%*&]).+$", message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character") String password,
+
+                LocalDate birthday) {
 }
