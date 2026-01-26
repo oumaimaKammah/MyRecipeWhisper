12427606b57392ace77bf0137251733e1c63a41d
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-01-26 14:25:44 +0100

    feat(dto): add light validation rules to UpdateUserDTO (username, email, birthday)

--- backend/src/main/java/com/myrecipewhisper/backend/dtos/UpdateUserDTO.java
+++ backend/src/main/java/com/myrecipewhisper/backend/dtos/UpdateUserDTO.java
@@ -2,9 +2,13 @@ package com.myrecipewhisper.backend.dtos;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public record UpdateUserDTO(
-        String username,
-        String email,
-        LocalDate birthday) {
+
+                @NotBlank String username,
+                @Email String email,
+                @NotBlank LocalDate birthday) {
 
 }
