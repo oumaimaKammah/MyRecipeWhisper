554ee01d3456626a597ef48364e8c09d81a51082
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-01-26 14:24:22 +0100

    feat(dto): add non-empty field validation to CreateUserDTO.

--- backend/src/main/java/com/myrecipewhisper/backend/dtos/CreateUserDTO.java
+++ backend/src/main/java/com/myrecipewhisper/backend/dtos/CreateUserDTO.java
@@ -14,5 +14,5 @@ public record CreateUserDTO(
 
                 @Size(min = 8, message = "Password must be at least 8 characters long") @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)(?=.*[@$!?#%*&]).+$", message = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character") String password,
 
-                LocalDate birthday) {
+                @NotBlank LocalDate birthday) {
 }
