b1af9ad9532476350df1fb84da21329999f2aaac
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-01-26 16:44:07 +0100

    feat(controller): add UserController with GET endpoint to retrieve all users.

---
+++ backend/src/main/java/com/myrecipewhisper/backend/controllers/UserController.java
@@ -0,0 +1,30 @@
+package com.myrecipewhisper.backend.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.myrecipewhisper.backend.services.impl.UserService;
+import com.myrecipewhisper.backend.dtos.UserDTO;
+import com.myrecipewhisper.backend.mappers.UserMapper;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public List<UserDTO> getAllUsers() {
+        return userService.getAllUsers()
+                .stream()
+                .map(UserMapper::toUserDTO)
+                .toList();
+    }
+}
