bd7d505e6d6fb7649bbe5a7b3b38da0cc53077f4
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-01-28 13:24:53 +0100

    feat(controller): update user endpoints and improve API responses.

--- backend/src/main/java/com/myrecipewhisper/backend/controllers/UserController.java
+++ backend/src/main/java/com/myrecipewhisper/backend/controllers/UserController.java
@@ -1,10 +1,22 @@
 package com.myrecipewhisper.backend.controllers;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
 
 import com.myrecipewhisper.backend.services.impl.UserService;
+
+import jakarta.validation.Valid;
+
+import com.myrecipewhisper.backend.dtos.CreateUserDTO;
+import com.myrecipewhisper.backend.dtos.UpdateUserDTO;
 import com.myrecipewhisper.backend.dtos.UserDTO;
 import com.myrecipewhisper.backend.mappers.UserMapper;
 
@@ -27,4 +39,30 @@ public class UserController {
                 .map(UserMapper::toUserDTO)
                 .toList();
     }
+
+    @GetMapping("/{id}")
+    public UserDTO getUserById(@PathVariable Integer id) {
+        var user = userService.getUserById(id);
+        return UserMapper.toUserDTO(user);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserDTO createUser(@Valid @RequestBody CreateUserDTO dto) {
+        var user = userService.createUser(dto);
+        return UserMapper.toUserDTO(user);
+    }
+
+    @PutMapping("/{id}")
+    public UserDTO updateUser(@PathVariable Integer id, @Valid @RequestBody UpdateUserDTO dto) {
+        var user = userService.updateUser(id, dto);
+        return UserMapper.toUserDTO(user);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteUser(@PathVariable Integer id) {
+        userService.deleteUserById(id);
+    }
+
 }
