80af09dfcb6e5f3726e2710fac231bc45756359f
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-01-28 16:09:11 +0100

    feat(repository) : add IngredientRepository for database access.

---
+++ backend/src/main/java/com/myrecipewhisper/backend/repositories/IngredientRepository.java
@@ -0,0 +1,11 @@
+package com.myrecipewhisper.backend.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.myrecipewhisper.backend.entities.Ingredient;
+
+public interface IngredientRepository extends JpaRepository<Ingredient, Integer> {
+    Optional<Ingredient> findByNameIgnoreCase(String name);
+}
