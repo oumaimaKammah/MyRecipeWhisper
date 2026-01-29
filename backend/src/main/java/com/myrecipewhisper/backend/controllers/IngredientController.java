88a1b2cc4deaacad01927e401f2cee19a7370e25
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-01-29 15:57:23 +0100

    refactor(ingredient) : delete unused imports.

--- backend/src/main/java/com/myrecipewhisper/backend/controllers/IngredientController.java
+++ backend/src/main/java/com/myrecipewhisper/backend/controllers/IngredientController.java
@@ -9,9 +9,6 @@ import com.myrecipewhisper.backend.services.impl.IngredientService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
