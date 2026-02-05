42d2094702ef23b19996e58776107849b68ac2ef
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-02-05 16:11:32 +0100

    feat(config) add SpoonacularProperties for external API configuration.

--- backend/src/main/java/com/myrecipewhisper/backend/configs/SpoonacularProperties.java
+++ backend/src/main/java/com/myrecipewhisper/backend/configs/SpoonacularProperties.java
@@ -1,37 +1,20 @@
 package com.myrecipewhisper.backend.configs;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
 
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter
-@Setter
-@Component
 @ConfigurationProperties(prefix = "spoonacular")
-public class SpoonacularProperties {
+public record SpoonacularProperties(
 
-    private Api api = new Api();
-    private Endpoints endpoints = new Endpoints();
-    private Defaults defaults = new Defaults();
+        Api api,
+        Endpoints endpoints) {
 
-    @Getter
-    @Setter
-    public static class Api {
-        private String key;
+    public record Api(
+            String key,
+            String host) {
+
     }
 
-    @Getter
-    @Setter
-    public static class Endpoints {
-        private String search;
-    }
-
-    @Getter
-    @Setter
-    public static class Defaults {
-        private int number;
-        private boolean addRecipeInformation;
+    public record Endpoints(
+            String search) {
     }
 }
