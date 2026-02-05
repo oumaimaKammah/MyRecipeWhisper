8d1a2aae5a330ef3abe7ffaacd296d8ed2ea947f
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-02-05 16:19:39 +0100

    refactor(dto): remove @NotNull constraint from cuisineId in RecipeSearchRequestDTO

--- backend/src/main/java/com/myrecipewhisper/backend/dtos/recipe/RecipeSearchRequestDTO.java
+++ backend/src/main/java/com/myrecipewhisper/backend/dtos/recipe/RecipeSearchRequestDTO.java
@@ -3,10 +3,9 @@ package com.myrecipewhisper.backend.dtos.recipe;
 import java.util.List;
 
 import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
 
 public record RecipeSearchRequestDTO(
-        @NotEmpty List<String> ingredients,
-        @NotNull Integer cuisineId) {
+                @NotEmpty List<String> ingredients,
+                Integer cuisineId) {
 
 }
