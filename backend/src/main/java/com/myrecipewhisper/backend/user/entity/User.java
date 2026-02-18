7f459230550a693a3d7cec3304a146b74314580d
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-02-18 12:34:02 +0100

    feat(user): update User entity to implement UserDetails for Spring security compatibilty.

--- backend/src/main/java/com/myrecipewhisper/backend/user/entity/User.java
+++ backend/src/main/java/com/myrecipewhisper/backend/user/entity/User.java
@@ -2,6 +2,11 @@ package com.myrecipewhisper.backend.user.entity;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import com.myrecipewhisper.backend.enums.AuthProvider;
 import com.myrecipewhisper.backend.enums.Role;
@@ -18,7 +23,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -51,4 +56,32 @@ public class User {
     @Column(name = "registered_at", nullable = false)
     private LocalDateTime registeredAt = LocalDateTime.now();
 
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
