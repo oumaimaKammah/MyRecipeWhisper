73f8fc8996e9c90848f087b97fe6d27dbd66e3de
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-02-18 12:57:04 +0100

    chore(security): remove unused import

--- backend/src/main/java/com/myrecipewhisper/backend/configs/security/SecurityConfig.java
+++ backend/src/main/java/com/myrecipewhisper/backend/configs/security/SecurityConfig.java
@@ -14,7 +14,6 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
 
