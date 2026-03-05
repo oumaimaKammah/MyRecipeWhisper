1d484d13faa3dba7a761be789b3cca82871357b2
oumaimaKammah <118013788+oumaimaKammah@users.noreply.github.com>
2026-03-05 23:03:08 +0100

    refactor(auth) : Updated register() to return RegisterResponseDTO instead of AuthResponseDTO

--- backend/src/main/java/com/myrecipewhisper/backend/auth/service/AuthService.java
+++ backend/src/main/java/com/myrecipewhisper/backend/auth/service/AuthService.java
@@ -3,10 +3,11 @@ package com.myrecipewhisper.backend.auth.service;
 import com.myrecipewhisper.backend.auth.dto.AuthResponseDTO;
 import com.myrecipewhisper.backend.auth.dto.LoginDTO;
 import com.myrecipewhisper.backend.auth.dto.RegisterDTO;
+import com.myrecipewhisper.backend.auth.dto.RegisterResponseDTO;
 
 public interface AuthService {
 
-    AuthResponseDTO register(RegisterDTO registerDTO);
+    RegisterResponseDTO register(RegisterDTO registerDTO);
 
     AuthResponseDTO login(LoginDTO loginDTO);
 
